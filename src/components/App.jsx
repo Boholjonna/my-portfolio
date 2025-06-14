10ce9abdf8b581703ac565de874a8e4d9ba84a07
Jonna Bohol BSCPE 1-G DAY PROGRAM <159886481+Boholjonna@users.noreply.github.com>
2025-06-14 18:58:11 +0800

    added layout for about section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,21 @@
-
 import '../styles/App.css'
-import Hello from './About'
+import About from './About'
+import msJ from '../images/msJ.png';
+import nightme from '../images/nightme.png';
 
 function App() {
+  const imageUrl = new URL('../images/msJ.png', import.meta.url).href
+  const profileUrl = new URL('../images/nightme.png', import.meta.url).href
 
-  const person = {
-    name: 'Jonna',
-    message: 'Hi there',
-    code: '1234',
-  };
-    
-
-  
   return (
     <div className="App">
-      <Hello message={person.message} name={person.name} code={person.code} />
+      <About 
+      intro="Hi I am Jonna Bohol, call me Ms J"
+        logo={imageUrl}
+        image={profileUrl}
+        title="a Computer Engineer"
+        message="I'm an enthusiastic and curious software and web developer with a passion for creating visually appealing, user-friendly websites and applications. I thrive on continuous learning and enjoy transforming ideas into impactful digital experiences. With strong time management and adaptability, I consistently deliver high-quality work on time. I'm driven by innovation and always eager to explore new trends, tools, and technologies to grow and refine my craft."
+      />
     </div>
   );
 }
