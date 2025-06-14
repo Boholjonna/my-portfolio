598c7119fcd20447b6f1247113cd71a560a3373b
MsJ <159886481+Boholjonna@users.noreply.github.com>
2025-06-14 14:17:59 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/',  // 👈 MUST match your GitHub repo name
+  base: '/my-portfolio/', 
   plugins: [react()],
 })
