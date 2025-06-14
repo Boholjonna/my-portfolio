0a9aed6b1fcb9678cf457b7eab5b572725bc06b1
Jonna Bohol BSCPE 1-G DAY PROGRAM <159886481+Boholjonna@users.noreply.github.com>
2025-06-14 19:47:56 +0800

    for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
- import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/',  
   plugins: [react()],
+  base: '/', // for root-level deployment
 })
+
